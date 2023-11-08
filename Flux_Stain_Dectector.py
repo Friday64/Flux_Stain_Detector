f8be5f304d72a71407fd85b65f74d6df1037ce8a
Friday64 <mcfrantz12@gmail.com>
2023-11-07 20:03:36 -0500

    Changes to entire program

--- Flux_Stain_Dectector.py
+++ Flux_Stain_Dectector.py
@@ -1,82 +1,58 @@
 import tkinter as tk
-from tkinter import filedialog
-from PIL import Image, ImageTk
-import numpy as np
+from tkinter import Frame, Label
 import cv2
-from tflite_runtime.interpreter import Interpreter, load_delegate
-
-# Initialize the TensorFlow Lite interpreter
-model_path = 'C:/Users/Matthew/Desktop/Flux_Models/Flux_Stain_Detector.tflite'
-interpreter = Interpreter(model_path=model_path, experimental_delegates=[load_delegate('libedgetpu.so.1.0')])
-interpreter.allocate_tensors()
-input_details = interpreter.get_input_details()
-output_details = interpreter.get_output_details()
-
-def preprocess_image(image_path, img_size=(28, 28)):
-    # Load the image
-    img = cv2.imread(image_path, cv2.IMREAD_GRAYSCALE)
-    img = cv2.resize(img, img_size)
-    img = img / 255.0  # Normalize the image
-    img = np.expand_dims(img, axis=-1)
-    img = np.expand_dims(img, axis=0).astype(np.float32)
-    return img
-
-def run_inference(image_path):
-    preprocessed_image = preprocess_image(image_path)
-    interpreter.set_tensor(input_details[0]['index'], preprocessed_image)
-    interpreter.invoke()
-    output_data = interpreter.get_tensor(output_details[0]['index'])
-    return output_data
-
-def select_image():
-    global image_path  # Declare 'image_path' as global to modify it
-
-    # Use file dialog to select an image file
-    file_path = filedialog.askopenfilename()
-    if file_path:
-        # Set the global 'image_path' to the selected file path
-        image_path = file_path
-
-        # Load and display the image
-        img = Image.open(file_path)
-        img.thumbnail((200, 200), Image.ANTIALIAS)  # Resize to fit the GUI
-        img = ImageTk.PhotoImage(img)
-        panel.configure(image=img)
-        panel.image = img
-        # Update the window title with the image file name
-        root.title(f"Flux Stain Detector - {file_path}")
+from PIL import Image, ImageTk
 
 
-def on_run_inference():
-    global image_path  # Access the global variable 'image_path'
+# Create a window
+window = tk.Tk()
+window.title("Flux Stain Detector")
 
-    if not image_path:
-        result_label.config(text="Please select an image first.")
-        return
+# Create a frame for the video feed
+video_frame = Frame(window)
+video_frame.pack(padx=10, pady=10)
 
-    result = run_inference(image_path)
-    # Update the result label based on the inference
-    result_label.config(text=f"Result: {result}")
+# Label for displaying the video frames
+video_label = Label(video_frame)
+video_label.pack()
 
-# Initialize the main window
-root = tk.Tk()
-root.title("Flux Stain Detector")
+# Function to update the video label with the latest frame
+def update_video_label(frame):
+    # Convert the image to a format that Tkinter can use
+    cv_image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+    pil_image = Image.fromarray(cv_image)
+    tk_image = ImageTk.PhotoImage(image=pil_image)
+    
+    # Update the label with the new image
+    video_label.imgtk = tk_image
+    video_label.configure(image=tk_image)
 
-# Add a button to select an image
-btn_select = tk.Button(root, text="Select Image", command=select_image)
-btn_select.pack()
+# Initialize the USB webcam feed
+camera = cv2.VideoCapture(0)
 
-# Add a panel to display the selected image
-panel = tk.Label(root)
-panel.pack()
+camera.set(cv2.CAP_PROP_FRAME_WIDTH, 1920)
+camera.set(cv2.CAP_PROP_FRAME_HEIGHT, 1080)
 
-# Add a button to run inference
-btn_run = tk.Button(root, text="Run Inference", command=on_run_inference)
-btn_run.pack()
 
-# Add a label to show the results
-result_label = tk.Label(root, text="")
-result_label.pack()
+def show_frame():
+    # Capture frame-by-frame
+    ret, frame = camera.read()
 
-# Start the GUI event loop
-root.mainloop()
+    if ret:
+        # Here you would add your model prediction and annotation code
+
+        # Update the GUI with the new frame
+        update_video_label(frame)
+
+    # Call this function again after a short delay to update the video feed
+    window.after(10, show_frame)
+
+# Start the frame update loop
+show_frame()
+
+# Run the GUI main loop
+window.mainloop()
+
+# Release the video capture when the GUI is closed
+camera.release()
+
